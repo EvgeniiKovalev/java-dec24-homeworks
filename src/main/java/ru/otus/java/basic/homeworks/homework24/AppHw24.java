63be7d37b0039259022a69f1808f9f61a39ad0cb
Evgeniy Kovalev <ev-kov@mail.ru>
2025-04-12 09:42:22 +0300

    ДЗ26 init

--- src/main/java/ru/otus/java/basic/homeworks/homework24/AppHw24.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.java.basic.homeworks.homework24;
-
-public class AppHw24 {
-    public static void main(String[] args) throws Exception {
-        System.out.println("AppHw24");
-    }
-}
