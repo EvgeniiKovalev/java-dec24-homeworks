07c64fbbb722b4c86234263d1fa19910b0c8bb1e
Evgeniy Kovalev <ev-kov@mail.ru>
2025-04-13 06:13:40 +0300

    ДЗ27 готово

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework27/AppHw27.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homeworks.homework27;
+
+public class AppHw27 {
+    public static void main(String[] args) {
+        StudyGenerics test = new StudyGenerics();
+        test.test1();
+    }
+}
